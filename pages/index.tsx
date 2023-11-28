16c656239940baf7998565dd4a854b94f37f43ba
RAHUL C H <46349786+sudotturi@users.noreply.github.com>
2023-11-28 15:12:53 +0000

    auth is done

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,97 +1,42 @@
-import Layout from "../components/layout";
-import { useEffect, useState } from 'react'
-import { ISudoTodo } from '../utils/constants'
+import Layout from "@/components/layouts/mainLayout";
+import { signIn, signOut, useSession } from "next-auth/react";
+import { ReactElement, useEffect, useState } from "react";
 
-export default function Home() {
-
-  const [data, setData] = useState<Array<ISudoTodo>>([]);
-  const [itemValue, setItemValue] = useState("");
-  const [reload, setReload] = useState(true);
-  const [isMounted, setMounted] = useState(false);
+export default function Home({}) {
+  const se = useSession();
+  console.log(se)
+  const [logged, setLogged] = useState(false);
   useEffect(() => {
-
-  }, [reload]);
-
-  useEffect(() => {
-    var sData = localStorage.getItem("TODO_LIST");
-    var pData = JSON.parse(sData || JSON.stringify([{ text: "Read a book", dateTime: new Date(), done: false }]));
-    setData(pData);
-    setMounted(true);
-  }, [])
-
-  const addData = () => {
-    if (!!itemValue) {
-      var dataDup = data;
-      dataDup.push({ text: itemValue, dateTime: new Date(), done: false });
-      setData(dataDup);
-      localStorage.setItem("TODO_LIST", JSON.stringify(dataDup));
-      setItemValue("");
-      setReload(!reload)
+    console.log(se.status)
+    if(se.status == 'loading'){
+      console.log("hahahah")
+    }else if(se.status == 'authenticated'){
+      setLogged(true);
     }
+  },[se.status]);
+  if(se.status == 'loading'){
+    return <p>loading</p>
   }
+  return (
+   <div>
+    test
+    {logged? (
+ <button onClick={() => signOut()}>Sign out</button>
+    ): (<button onClick={() => signIn("credentials", {
+      redirect: false,
+      email: 'superadmin@sudofolks.com',
+      password: 'test',
+      callbackUrl:'/',
+    })}>Sign In</button>)}
+   </div>
+  )
+}
 
-  const onKey = (key: string) => {
-    if (key == "Enter")
-      addData();
-  }
 
-
-  const deleteItem = (ind: number) => {
-    var dataDup = data;
-    dataDup.splice(ind, 1);
-    setData(dataDup);
-    localStorage.setItem("TODO_LIST", JSON.stringify(dataDup));
-    setReload(!reload);
-  }
-  
-  const updateDone = (checked: any, index: number) => {
-    var dataDup = data;
-    if (checked.value) {
-      dataDup[index].done = true;
-      setData(dataDup);
-      localStorage.setItem("TODO_LIST", JSON.stringify(dataDup));
-      setReload(!reload);
-    }
-  }
-
+Home.getLayout = function getLayout(page: ReactElement) {
   return (
     <Layout>
-      <main className='todo-main'>
-        <div className="container app-container bg-gray-200 dark:bg-gray-800" id="taskList">
-          <h1 className="dark:text-gray-200 text-gray-900 text-xl">TO DO LIST</h1>
-          <div className="add-task">
-            <input
-              type="text"
-              autoComplete="off"
-              placeholder="Add New Task"
-              className="task-input text-blue-900 dark:text-gray-100"
-              value={itemValue}
-              onChange={(e) => setItemValue(e.target.value)}
-              onKeyUp={(e) => onKey(e.code)}
-            />
-            <button
-              type="button"
-              defaultValue=""
-              className="submit-task"
-              title="Add Task"
-              onClick={() => addData()}
-            />
-          </div>
-
-          <ul className="task-list">
-            {data?.map((item, index) => (
-              <li className="task-list-item" v-for="task in tasks" key={index}>
-                <label className="task-list-item-label">
-                  <input type="checkbox" onChange={(e) => updateDone(e.target, index)} defaultChecked={item?.done} />
-                  <span>{item?.text}</span>
-                </label>
-                <button className="delete-btn" title="Delete Task" onClick={() => deleteItem(index)} />
-              </li>
-            ))}
-          </ul>
-        </div>
-
-      </main>
+      {page}
     </Layout>
   )
 }
--- prisma/seed.ts
+++ prisma/seed.ts
@@ -5,19 +5,21 @@ const prisma = new PrismaClient()
 
 const userData: Prisma.UserCreateInput[] = [
   {
-    email: 'jesse@mongodb.com',
-    username: 'jesse',
-    passwordHash: 'sdfsfsf'
+    email: 'superadmin@sudofolks.com',
+    username: 'superadmin',
+    passwordHash: '$2a$12$rq6BZ0NNJTcg9Ma.WuTxa.JYgtUYZUg5Ex0NcIkpzpc7n/KL1OPXu' // test
   }
 ]
 
 async function main() {
   console.log(`Start seeding ...`)
   for (const u of userData) {
+    const del = await prisma.user.delete({where: {email:u.email}})
+    console.log("deleting seed user :" + del.id);
     const user = await prisma.user.create({
       data: u,
     })
-    console.log(`Created user with id: ${user}`)
+    console.log(`Created user with id: ${user.email}`)
   }
   console.log(`Seeding finished.`)
 }
