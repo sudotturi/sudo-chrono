43c1ff577f9bf061bf203c99745194a6da579490
Rahul C H <rahul.ch@nutanix.com>
2023-12-14 21:54:43 +0530

    dev

--- pages/teams/index.js
+++ pages/teams/index.js
@@ -55,7 +55,7 @@ export default function Team({ feed, setLoading }) {
             <button
               type="button"
               onClick={() => openAddUserModel()}
-              className=" uppercase flex items-center justify-center px-4 py-2 text-sm font-medium text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800"
+              className=" uppercase flex items-center justify-center px-4 py-2 text-sm font-medium text-white rounded-lg bg-cyan-700 hover:bg-cyan-800 focus:ring-4 focus:ring-cyan-300 dark:bg-cyan-600 dark:hover:bg-cyan-700 focus:outline-none dark:focus:ring-cyan-800"
             >
               <PlusIcon className="h-3.5 w-3.5 mr-2" />
               Add New Member
